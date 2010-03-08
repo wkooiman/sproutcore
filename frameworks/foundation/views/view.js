53d0bb7007eab1daf1829bc13e02e70121283d48
Alex Iskander <alex@tpsitulsa.com>
2010-03-08 13:20:05 -0600

    Stop using isFirstTouch/isLastTouch (problematic) and instead just set view.hasTouch and let the pane handle the rest as needed.

--- frameworks/foundation/views/view.js
+++ frameworks/foundation/views/view.js
@@ -318,6 +318,11 @@ SC.View = SC.Responder.extend(SC.DelegateSupport,
   */
   acceptsMultitouch: NO,
   
+  /**
+    Is YES if the view is currently being touched. NO otherwise.
+  */
+  hasTouch: NO,
+  
   // ..........................................................
   // IS VISIBLE IN WINDOW SUPPORT
   // 
