5fb660c8fa81ae80561e44e5adcad9d94daae866
Juan Pinzon <juanpin@Juan-Pinzons-MacBook-Pro.local>
2010-03-26 01:31:30 -0700

    Partial revert that gets keyhandlers working as expected once again for apps containing iframes

--- frameworks/desktop/system/root_responder.js
+++ frameworks/desktop/system/root_responder.js
@@ -524,6 +524,7 @@ SC.RootResponder = SC.RootResponder.extend(
   
   mousedown: function(evt) {
     try {
+
       window.focus();
       // First, save the click count. The click count resets if the mouse down
       // event occurs more than 200 ms later than the mouse up event or more
