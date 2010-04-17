2fe71d96ef8a073d6dc2ffc64bd6dd1b0c919e31
Alex Iskander <alex@tpsitulsa.com>
2010-04-17 17:49:58 -0500

    Root responder should not send touchStart to a view already in the responder stack. This probably caused a few ScrollView bugs.

--- frameworks/foundation/system/root_responder.js
+++ frameworks/foundation/system/root_responder.js
@@ -862,12 +862,16 @@ SC.RootResponder = SC.Object.extend({
     if (responder) pane = responder.get('pane') ;
     else pane = this.get('keyPane') || this.get('mainPane') ;
 
-    // if we found a valid pane, send the event to it
-    try {
-      responder = (pane) ? pane.sendEvent("touchStart", touch, responder) : null ;
-    } catch (e) {
-      SC.Logger.error("Error in touchStart: " + e);
-      responder = null;
+    // if the responder is not already in the stack...
+    
+    if (stack.indexOf(responder) < 0) {
+      // if we found a valid pane, send the event to it
+      try {
+        responder = (pane) ? pane.sendEvent("touchStart", touch, responder) : null ;
+      } catch (e) {
+        SC.Logger.error("Error in touchStart: " + e);
+        responder = null;
+      }
     }
 
     // and again, now that we have more detail.
